45e3757530327795ccbf8d702216e54518f5d706
Nicola Gaioni <nicog.designer@gmail.com>
2021-01-21 16:49:33 +0100

    21.01

--- public/script.js
+++ public/script.js
@@ -1,18 +1,46 @@
 const canvas = $("canvas");
-const picture = canvas.getContext("2d");
+// console.log(canvas);
+const picture = canvas[0].getContext("2d");
 
-let drawing = false;
+let isDrawing = false;
 let x = 0;
 let y = 0;
 
-draw = (picture, x1, y1, x2, y2) => {
+drawing = (picture, xA, yA, xB, yB) => {
     picture.strokeStyle = "white";
     picture.lineWidth = 1;
 
     picture.beginPath();
-    picture.moveTo;
+    picture.moveTo(xA, yA);
+    picture.lineTo(xB, yB);
+    picture.stroke();
+    picture.closePath();
 };
 
-canvas.on("mousedown", draw);
-canvas.on("mousemove");
-window.on("mouseup");
+draw = (e) => {
+    isDrawing = true;
+    x = e.offsetX;
+    y = e.offsetY;
+};
+
+canvas.on("mousedown", (e) => {
+    console.log("mousedown!");
+    draw(e);
+});
+canvas.on("mousemove", (e) => {
+    if (isDrawing === true) {
+        console.log("mousemove!");
+        drawing(picture, x, y, e.offsetX, e.offsetY);
+        x = e.offsetX;
+        y = e.offsetY;
+    }
+});
+$(window).on("mouseup", (e) => {
+    if (isDrawing === true) {
+        console.log("mouseup!");
+        drawing(picture, x, y, e.offsetX, e.offsetY);
+        isDrawing = false;
+        x = 0;
+        y = 0;
+    }
+});
